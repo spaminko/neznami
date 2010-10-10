9dc83605c37879e7e9f17655007309b43878a8ce
spaminko <spaminko@zoznam.sk>
2010-10-10 11:45:11 +0200

    added price to product table

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20101010093832) do
+
+  create_table "products", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "img_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.decimal  "price",       :precision => 8, :scale => 2, :default => 0.0
+  end
 
 end
