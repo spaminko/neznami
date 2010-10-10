3689c3ced145dd769cc82973b3b34cd9cf3a2606
spaminko <spaminko@zoznam.sk>
2010-10-10 11:59:50 +0200

    added validation to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ActiveRecord::Base
+  validates_presence_of :title, :description, :img_url
+  validates_numericality_of :price
+  validate :the_price_must_be_at_least_cent
+  validates_uniqueness_of :title
+  validates_format_of :img_url,
+                      :with => %r{\.(gif|jpg|png)$}i,
+                      :message => 'must be a URL for GIF, JPG ' +
+                              'or PNG image.'
+
+  protected
+  def the_price_must_be_at_least_cent
+    errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
+  end
 end
